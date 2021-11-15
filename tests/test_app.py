3804679d5d19a3c0eb40a60bab23edb2f5703666
Kahcho1 <91483569+Kahcho1@users.noreply.github.com>
2021-11-15 14:04:38 +0000

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -84,7 +84,7 @@ class TestComp(TestBase):
             url_for("status", id=1),
             follow_redirects=True
         )
-        self.assertEqual(Tasks.query.get(1).complete, True)
+        self.assertEqual(Tasks.query.get(1).completed, True)
 
 class TestIncomp(TestBase):
     def test_Incomp_task(self):
