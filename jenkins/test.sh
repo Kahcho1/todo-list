e250fdf6be843112efc1d54c27a33e50698dacdf
Ubuntu <kaho@dev-jenkins.x5jitdqegk3uxjg0e5j5gsbjld.cwx.internal.cloudapp.net>
2021-11-26 14:16:08 +0000

    testing v1.2

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Test Stage"
+
+pip3 install pytest pytest-cov
+
 python3 -m pytest \
     --cov=application \
     --cov-report term-missing \
     --cov-report xml:coverage.xml \
     --junitxml=junit_report.xml
-    
