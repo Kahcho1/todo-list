81395a8367b032b02eec515fe4825234f6a2c207
Ubuntu <kaho@dev-jenkins.x5jitdqegk3uxjg0e5j5gsbjld.cwx.internal.cloudapp.net>
2021-11-26 11:05:37 +0000

    adding deploy stage v3.4

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -5,4 +5,6 @@ echo "Deploy Stage"
 scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh jenkins@swarm-manager \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    DATABASE_URI=$DATABASE_URI \
+    CREATE_SCHEMA=$CREATE_SCHEMA \
     docker stack deploy --compose-file docker-compose.yaml todo-list
