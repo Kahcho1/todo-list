f45a480f1a90585e45e4003e5534a413ca25f9f5
Ubuntu <kaho@dev-jenkins.x5jitdqegk3uxjg0e5j5gsbjld.cwx.internal.cloudapp.net>
2021-11-26 13:52:41 +0000

    testing

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Test Stage"
+python3 -m pytest \
+    --cov=application \
+    --cov-report term-missing \
+    --cov-report xml:coverage.xml \
+    --junitxml=junit_report.xml
+    
