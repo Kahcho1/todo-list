363cd6c4c77f24e2c3c73db43b20fee9f39302eb
Ubuntu <kaho@dev-jenkins.x5jitdqegk3uxjg0e5j5gsbjld.cwx.internal.cloudapp.net>
2021-11-29 10:52:56 +0000

    testing stage v2

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -2,12 +2,28 @@
 
 echo "Test Stage"
 
-# install pytest
-pip install pytest pytest-cov
+# venv created + source
+python3 -m venv venv
+source venv/bin/activate
 
-# testing
-python3 -m pytest \
-    --cov=application \
-    --cov-report term-missing \
-    --cov-report xml:coverage.xml \
-    --junitxml=junit_report.xml
+# install pytest, flask_testing and requirements for frontend and backend
+pip install pytest pytest-cov flask_testing
+pip3 install -r frontend/requirements.txt
+pip3 install -r frontend/requirements.txt
+
+# run pytest frontend
+python3 -m pytest frontend
+
+# run pytest frontend
+python3 -m pytest backend
+
+# remove venv
+deactivate
+rm -rf venv
+
+# # testing
+# python3 -m pytest \
+#     --cov=application \
+#     --cov-report term-missing \
+#     --cov-report xml:coverage.xml \
+#     --junitxml=junit_report.xml
